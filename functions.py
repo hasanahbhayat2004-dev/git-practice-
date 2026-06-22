2459f3449ded6c319ed52f6092d5bd9c99bb2b4b
Hasanah Bhayat <hasanahbhayat@Hasanahs-MacBook-Air.local>
2026-06-22 08:50:46 +0100

    Implement get_initials

--- functions.py
+++ functions.py
@@ -1,2 +1,6 @@
 def is_even(number):
     return number % 2 == 0 
+
+def get_initials(name):
+    names = name.split()
+    return names[0][0] + names[1][0]
--- test_functions.py
+++ test_functions.py
@@ -1,4 +1,13 @@
 from functions import is_even
+
 def test_is_even():
     assert is_even(4) == True
     assert is_even(5) == False
+
+from functions import get_initials
+
+def test_get_initials():
+    assert get_initials("Hasanah Bhayat") == "HB"
+    
+def test_get_initials_john():
+    assert get_initials("John Smith") == "JS"
